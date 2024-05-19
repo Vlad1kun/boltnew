236716e9024d44e2caeba4bf6ca42e9f48590001
Vlad1kun <109257662+Vlad1kun@users.noreply.github.com>
2024-05-19 23:33:48 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,127 @@ import { RectAreaLightHelper } from 'RectAreaLightHelper'
 import { RectAreaLightUniformsLib } from 'RectAreaLightUniformsLib';
 import { Scene } from 'three';
 
+// old code
 
+let coin = document.querySelector(".coin");
+let point = document.querySelector(".Point");
+let energy = document.querySelector(".Energy");
+let slesh = document.querySelector(".slesh");
+
+let close = document.querySelector(".close");
+let windowMod = document.querySelector(".window");
+
+let boosts_button = document.querySelector(".boosts_button");
+let pointBoost = document.querySelector(".pointBoost");
+
+let tapLvl = document.querySelector(".tapLvl");
+let regenLvl = document.querySelector(".regenLvl");
+let massLvl = document.querySelector(".massLvl");
+
+let priceTap = document.querySelector(".priceTap");
+let priceRegen = document.querySelector(".priceRegen");
+let priceMass = document.querySelector(".priceMass");
+
+
+let pointSave = localStorage.getItem('pointSave') || 0;
+point.innerHTML = Number(pointSave);
+let clickLVL = localStorage.getItem('clickLVL') || 0;
+let energyRegenLVL = localStorage.getItem('energyRegenLVL') || 0;
+let energyMaxLVL = localStorage.getItem('energyMaxLVL') || 0;
+
+let clickPoints = Number(localStorage.getItem('clickPoints')) || 1;
+let energyRegen = Number(localStorage.getItem('energyRegen')) || 1;
+let energyMax = Number(localStorage.getItem('energyMax')) || 500;
+
+let clickSell = localStorage.getItem('clickSell') || 100;
+let regenSell = localStorage.getItem('regenSell') || 100;
+let massEnergySell = localStorage.getItem('massEnergySell') || 100;
+
+// localStorage.clear()
+
+
+// let currentEnergy = energyMax;
+let currentEnergy = localStorage.getItem('currentEnergy') || 500;
+energy.textContent = Number(currentEnergy);
+
+
+let clickApp = document.querySelector(".clickApp")
+let RegenApp = document.querySelector(".RegenApp")
+let MassEnergyApp = document.querySelector(".MassEnergyApp")
+
+slesh.textContent = '/' + energyMax
+
+// close.addEventListener("click", () => { 
+//     windowMod.style.display = "none";
+    
+// }
+// )
+
+// boosts_button.addEventListener("click", () => { 
+//     windowMod.style.display = "block";
+//     pointBoost.textContent = pointSave + " BLTC";
+//     tapLvl.textContent = clickLVL;
+//     regenLvl.textContent = energyRegenLVL;
+//     massLvl.textContent = energyMaxLVL;
+//     priceTap.textContent = clickSell;
+//     priceRegen.textContent = regenSell;
+//     priceMass.textContent = massEnergySell;
+    
+    
+// }
+// )
+
+// Сохраняем текущее время при выходе из приложения
+window.addEventListener('unload', function() {
+    let exitTime = new Date().getTime();
+    localStorage.setItem('exitTime', exitTime);
+});
+
+// Восстанавливаем энергию при входе в приложение
+window.addEventListener('load', function() {
+    let enterTime = new Date().getTime();
+    let exitTime = localStorage.getItem('exitTime');
+    
+    if (exitTime) {
+        let timeDifference = enterTime - exitTime; // Время в миллисекундах
+        let energyToRegenerate = Math.floor(timeDifference / 1000) * energyRegen; // Предполагая, что энергия восстанавливается каждую секунду
+
+        currentEnergy = Number(currentEnergy) + energyToRegenerate;
+        if (currentEnergy > energyMax) {
+            currentEnergy = energyMax;
+        }
+
+        localStorage.setItem('currentEnergy', currentEnergy);
+        energy.textContent = Number(currentEnergy);
+    }
+});
+
+
+//
+
+// Функция для восстановления энергии
+
+function regenerateEnergy() {
+    currentEnergy = Number(currentEnergy);
+    if (currentEnergy + energyRegen >= energyMax) {
+        currentEnergy = energyMax; // Устанавливаем максимальное значение
+        localStorage.setItem('currentEnergy', currentEnergy);
+        energy.textContent = currentEnergy;
+        
+    } else if (currentEnergy + energyRegen < energyMax) {
+        currentEnergy += energyRegen; // Восстанавливаем энергию
+        localStorage.setItem('currentEnergy', currentEnergy);
+        energy.textContent = Number(currentEnergy);
+        
+    }
+    localStorage.setItem('currentEnergy', currentEnergy);
+    energy.textContent = Number(currentEnergy); // Обновляем отображение энергии
+}
+
+// Устанавливаем интервал для восстановления энергии
+setInterval(regenerateEnergy, 1000);
+
+//old code end
 
 // Создаем сцену
 var scene = new THREE.Scene();
@@ -99,6 +219,88 @@ function onTouchStart(event) {
         // Определяем направление вращения на основе позиции касания
         var directionX = (touch.x > 0) ? 1 : -1;
         var directionY = (touch.y > 0) ? -1 : 1;
+        // old code
+
+        if (Number(currentEnergy) > 0 && Number(currentEnergy) - clickPoints > 0) {
+            point.innerHTML = Number(point.textContent) + clickPoints;
+            pointSave = point.innerHTML
+            currentEnergy -= clickPoints; // Отнимаем энергию при клике
+            energy.textContent = Number(currentEnergy);
+            localStorage.setItem('pointSave', pointSave);
+        
+            // Создаем элемент для анимации
+            let scoreElement = document.createElement('div');
+            scoreElement.textContent = '+' + clickPoints;
+            scoreElement.style.position = 'absolute';
+            scoreElement.style.left = event.touches[0].clientX + 'px'; // Используем координаты касания
+            scoreElement.style.top = event.touches[0].clientY + 'px'; // Используем координаты касания
+            scoreElement.style.zIndex = "10";
+            scoreElement.style.backgroundImage = "url('assets/btc.svg')"
+            scoreElement.style.width = "30px"
+            scoreElement.style.height = "15px"
+            scoreElement.style.backgroundPosition = "left"
+            scoreElement.style.paddingLeft = "13px"
+            scoreElement.style.backgroundSize = "13px 13px"
+            scoreElement.style.backgroundRepeat = "no-repeat"
+            scoreElement.style.display = "block"
+            document.body.appendChild(scoreElement);
+        
+            // Добавляем анимацию
+            scoreElement.animate([
+                { transform: 'translateY(0px)', opacity: 1 },
+                { transform: 'translateY(-50px)', opacity: 0 }
+            ], {
+                duration: 1000,
+                easing: 'ease-out'
+            });
+        
+            // Удаляем элемент после анимации
+            setTimeout(() => {
+                document.body.removeChild(scoreElement);
+            }, 1000);
+
+            
+        } else if (Number(currentEnergy) - clickPoints < 0) {
+            point.innerHTML = Number(point.textContent) + Number(currentEnergy);
+            pointSave = point.innerHTML
+            currentEnergy -= Number(currentEnergy); // Отнимаем энергию при клике
+            energy.textContent = Number(currentEnergy);
+            localStorage.setItem('pointSave', pointSave);
+    
+            // Создаем элемент для анимации
+            let scoreElement = document.createElement('div');
+            scoreElement.textContent = '+' + clickPoints;
+            scoreElement.style.position = 'absolute';
+            scoreElement.style.left = event.touches[0].clientX + 'px'; // Используем координаты касания
+            scoreElement.style.top = event.touches[0].clientY + 'px'; // Используем координаты касания
+            scoreElement.style.zIndex = "10";
+            scoreElement.style.backgroundImage = "url('assets/btc.svg')"
+            scoreElement.style.width = "30px"
+            scoreElement.style.height = "15px"
+            scoreElement.style.backgroundPosition = "left"
+            scoreElement.style.paddingLeft = "13px"
+            scoreElement.style.backgroundSize = "13px 13px"
+            scoreElement.style.backgroundRepeat = "no-repeat"
+            scoreElement.style.display = "block"
+            document.body.appendChild(scoreElement);
+    
+            // Добавляем анимацию
+            scoreElement.animate([
+                { transform: 'translateY(0px)', opacity: 1 },
+                { transform: 'translateY(-50px)', opacity: 0 }
+            ], {
+                duration: 1000,
+                easing: 'ease-out'
+            });
+    
+            // Удаляем элемент после анимации
+            setTimeout(() => {
+                document.body.removeChild(scoreElement);
+            }, 1000);
+    
+        }
+
+        //old code end
     
         // Создаем новый tween для вращения модели
         new TWEEN.Tween(intersects[0].object.rotation)
@@ -131,4 +333,3 @@ animate();
 
 // Функция анимации
 
-    
